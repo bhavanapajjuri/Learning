69dec5c2b21cf3160ca1b986b4d4e7d8c6848b23
bhavanapajjuri <bhavanapajjuri93@gmail.com>
2020-08-14 11:30:08 +0430

    adding few py files

---
+++ FormattedString.py
@@ -0,0 +1,9 @@
+first = 'Bhavana'
+last = 'Pajjuri'
+#I want to print Bhavana [Pajjuri] is a coder
+#writing without using formatted string
+msg = first + ' ['+last+'] is a coder'
+print(msg)
+#writing using formated string
+message = f'{first} [{last}] is a coder'
+print("printing message - "+message)
---
+++ String3.py
@@ -0,0 +1,11 @@
+sentence ='''this is
+Bhavana Pajjuri
+and I am learning Python'''
+print(sentence[-8])
+print(sentence)
+print(sentence[0:6])#prints characters from 0 to 4 and excludes 6
+sen = sentence[:]
+print(sen)
+
+name ="Bhavana"
+print("the name is - "+name[1:-2])#starts from first and ends at 2 nd index from last and prints the charcters in between them
